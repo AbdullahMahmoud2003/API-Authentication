1293117d41701d18364acd8a1355c44cf48f75ac
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-03-24 22:34:20 +0200

    preparing for postman work

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const app = express();
 //import routes
 const authRoute = require('./routes/auth');
 
+//Route middlewares
+app.use('/api/user', authRoute)
+
 app.listen(3000, () => console.log("Server is Up"));
