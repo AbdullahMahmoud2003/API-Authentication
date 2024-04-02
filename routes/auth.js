4c5f2ad22f0d7711f0ec7f60dfcf50f1aaaa152e
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-04-02 21:51:56 +0200

    adding the validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const User = require('../Models/User');
 
+//validation 
+const Joi = require("joi");
+const Schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', async (req, res) => {
+
+    //validate the user
+    const {error} = Schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -9,10 +22,10 @@ router.post('/register', async (req, res) => {
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send(savedUser)
     }catch(err){
         res.status(400).send(err);
-    };
+    }
 
 })
 
