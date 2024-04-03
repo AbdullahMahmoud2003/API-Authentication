77146bbf4abbde5af95c6bd358dbb910670df316
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-04-03 23:13:11 +0200

    solving the same email register error

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,14 @@ const {regiterValidation} = require('./validation')
 router.post('/register', async (req, res) => {
 
     //validate the user
+    const {error} = regiterValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
+    
+    //checking if the user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send("email already exists");
 
+    //create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
