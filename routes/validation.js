2d87bc5419d8241f805d50c22ac3615c6a206f78
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-04-03 23:05:36 +0200

    addin login validation function

--- routes/validation.js
+++ routes/validation.js
@@ -10,4 +10,13 @@ const regiterValidation = (data) =>{
     return Schema.validate(data);
 }
 
+const loginValidation = (data) =>{
+    const Schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return Schema.validate(data);
+}
+
 module.exports.regiterValidation = regiterValidation;
+module.exports.loginValidation = loginValidation;
